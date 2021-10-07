f1df4dcc8554cf671069cbd004a0852b5818ef64
Scott Shawcroft <scott@tannewt.org>
2021-10-07 16:58:19 -0700

    More interrupt and USB work

--- gen_svd.py
+++ gen_svd.py
@@ -1,9 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 import pathlib
 
-env = Environment(
-    loader=FileSystemLoader(searchpath=["svd"])
-)
+env = Environment(loader=FileSystemLoader(searchpath=["svd"]))
 
 bcm2711 = env.get_template("chips/bcm2711.svd.jinja")
 
@@ -12,9 +10,11 @@ gen = pathlib.Path("svd/gen")
 
 bcm2711_altfunc = [[None] * 6 for i in range(58)]
 
+
 def i2c(start_pin, altfunc, instance_number):
     bcm2711_altfunc[start_pin][altfunc] = f"SDA{instance_number}"
     bcm2711_altfunc[start_pin + 1][altfunc] = f"SCL{instance_number}"
+
 
 i2c(0, 0, 0)
 i2c(2, 0, 1)
@@ -30,11 +30,13 @@ i2c(28, 0, 0)
 i2c(44, 1, 0)
 i2c(44, 2, 1)
 
+
 def spi(start_pin, altfunc, instance_number):
     bcm2711_altfunc[start_pin][altfunc] = f"SPI{instance_number}_CE0_N"
     bcm2711_altfunc[start_pin + 1][altfunc] = f"SPI{instance_number}_MISO"
     bcm2711_altfunc[start_pin + 2][altfunc] = f"SPI{instance_number}_MOSI"
     bcm2711_altfunc[start_pin + 3][altfunc] = f"SPI{instance_number}_SCLK"
+
 
 spi(0, 3, 3)
 spi(4, 3, 4)
@@ -50,15 +52,17 @@ for i, instance in enumerate(range(3, 7)):
 bcm2711_altfunc[35][0] = "SPI0_CE1_N"
 spi(36, 0, 0)
 
+
 def in_order(start_pin, altfunc, prefix, suffixes):
     for i, suffix in enumerate(suffixes):
         if suffix is None:
             continue
         bcm2711_altfunc[start_pin + i][altfunc] = prefix + str(suffix)
 
+
 in_order(4, 2, "DPI_D", range(0, 24))
 in_order(8, 1, "SD", range(18))
-in_order(0, 1, "SA", range(5, -1 , -1))
+in_order(0, 1, "SA", range(5, -1, -1))
 in_order(6, 1, "", ("SOE_N", "SWE_N"))
 in_order(0, 2, "", ("PCLK", "DE", "LCD_VSYNC", "LCD_HSYNC"))
 in_order(4, 0, "GPCLK", range(3))
@@ -70,7 +74,7 @@ in_order(20, 5, "GPCLK", range(2))
 in_order(22, 0, "SD0_", ("CLK", "CMD", "DAT0", "DAT1", "DAT2", "DAT3"))
 in_order(22, 3, "SD1_", ("CLK", "CMD", "DAT0", "DAT1", "DAT2", "DAT3"))
 in_order(22, 4, "ARM_", ("TRST", "RTCK", "TDO", "TCK", "TDI", "TMS"))
-in_order(28, 1, "SA", range(5, -1 , -1))
+in_order(28, 1, "SA", range(5, -1, -1))
 in_order(28, 4, "MII_A_", ("RX_ERR", "TX_ERR", "CRS", "COL"))
 in_order(28, 5, "RGMII_", ("MDIO", "MDC"))
 in_order(32, 4, "SD_CARD_", ("PRES", "WRPROT", "LED"))
@@ -91,11 +95,13 @@ bcm2711_altfunc[44][0] = "GPCLK1"
 bcm2711_altfunc[45][0] = "PWM0_1"
 in_order(44, 5, "SD_CARD_", ("VOLT", "PWR0"))
 
+
 def uart(start_pin, altfunc, instance_number):
     bcm2711_altfunc[start_pin][altfunc] = f"TXD{instance_number}"
     bcm2711_altfunc[start_pin + 1][altfunc] = f"RXD{instance_number}"
     bcm2711_altfunc[start_pin + 2][altfunc] = f"CTS{instance_number}"
     bcm2711_altfunc[start_pin + 3][altfunc] = f"RTS{instance_number}"
+
 
 uart(0, 4, 2)
 uart(4, 4, 3)
@@ -116,4 +122,102 @@ uart(40, 5, 1)
 for i, row in enumerate(bcm2711_altfunc):
     print(i, repr(row))
 
-(gen / "bcm2711_lpa.svd").write_text(bcm2711.render(peripheral_base=0xFE000000, altfunc=bcm2711_altfunc, name="bcm2711_lpa"))
+videocore_interrupts = [
+    "Timer 0",
+    "Timer 1",
+    "Timer 2",
+    "Timer 3",
+    "H264 0",
+    "H264 1",
+    "H264 2",
+    "JPEG",
+    "ISP",
+    "USB",
+    "V3D",
+    "Transposer",
+    "Multicore Sync 0",
+    "Multicore Sync 1",
+    "Multicore Sync 2",
+    "Multicore Sync 3",
+    "DMA 0",
+    "DMA 1",
+    "DMA 2",
+    "DMA 3",
+    "DMA 4",
+    "DMA 5",
+    "DMA 6",
+    ("DMA_7_8", "OR of DMA 7 and 8"),
+    ("DMA_9_10", "OR of DMA 9 and 10"),
+    "DMA 11",
+    "DMA 12",
+    "DMA 13",
+    "DMA 14",
+    ("AUX", "OR of UART1, SPI1 and SPI2"),
+    "ARM",
+    "DMA 15",
+    "HDMI CEC",
+    "HVS",
+    "RPIVID",
+    "SDC",
+    "DSI 0",
+    "Pixel Valve 2",
+    "Camera 0",
+    "Camera 1",
+    "HDMI 0",
+    "HDMI 1",
+    "Pixel Valve 3",
+    "SPI/BSC Slave",
+    "DSI 1",
+    "Pixel Valve 0",
+    ("PIXEL_VALVE_1_2", "OR of Pixel Valve 1 and 2"),
+    "CPR",
+    "SMI",
+    "GPIO 0",
+    "GPIO 1",
+    "GPIO 2",
+    "GPIO 3",
+    ("I2C", "OR of all I2C"),
+    ("SPI", "OR of all SPI"),
+    "PCM/I2S",
+    "SDHOST",
+    ("UART", "OR of all PL011 UARTs"),
+    ("ETH_PCIE", "OR of all ETH_PCIe L2"),
+    "VEC",
+    "CPG",
+    "RNG",
+    ("EMMC", "OR of EMMC and EMMC2"),
+    "ETH_PCIe secure"
+]
+
+armc_interrupts = [
+    ("TIMER", "ARMC Timer"),
+    ("MAILBOX", "Mailbox"),
+    ("DOORBELL0", "Doorbell 0"),
+    ("DOORBELL1", "Doorbell 1"),
+    ("VPU0_HALTED", "VPU0 halted"),
+    ("VPU1_HALTED", "VPU1 halted"),
+    ("ARM_ADDRESS_ERROR", "ARM address error"),
+    ("ARM_AXI_ERROR", "ARM AXI error"),
+]
+
+for i in range(8):
+    armc_interrupts.append((f"SWI{i}", f"Software interrupt {i}"))
+
+interrupt_names = {}
+for i, irq in enumerate(armc_interrupts):
+    interrupt_names[64 + i] = irq
+for i, irq in enumerate(videocore_interrupts):
+    if isinstance(irq, str):
+        name = irq.replace(" ", "_").replace("/", "_").upper()
+        irq = (name, irq)
+    interrupt_names[96 + i] = irq
+
+(gen / "bcm2711_lpa.svd").write_text(
+    bcm2711.render(
+        peripheral_base=0xFE000000,
+        arm_local_base=0xFF800000,
+        altfunc=bcm2711_altfunc,
+        name="bcm2711_lpa",
+        interrupt_names=interrupt_names,
+    )
+)
