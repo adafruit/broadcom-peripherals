fe4fe13b4f999700e3797d3b8775250aaf68783b
Scott Shawcroft <scott@tannewt.org>
2021-12-01 14:41:05 -0800

    Finish pins and implement PACTL interrupt muxing

--- broadcom/pins.h
+++ broadcom/pins.h
@@ -26,6 +26,8 @@
 
 #pragma once
 
+#include "broadcom_peripherals_config.h"
+
 typedef enum {
     PIN_FUNCTION_NONE = 0,
     PIN_FUNCTION_I2C = 1,
@@ -54,6 +56,8 @@ typedef enum {
     UART_FUNCTION_RTS = 3
 } pin_uart_function_t;
 
+#define NONE_FUNCTION_0
+
 typedef struct {
     uint8_t type;
     uint8_t index;
@@ -63,5 +67,77 @@ typedef struct {
 typedef struct {
     PIN_PREFIX_FIELDS
     uint8_t number;
-    pin_function_t[6] functions;
+    pin_function_t functions[6];
 } mcu_pin_obj_t;
+
+#if BCM_VERSION == 2711
+#define BCM_PIN_COUNT (58)
+#else
+#define BCM_PIN_COUNT (54)
+#endif
+
+extern const mcu_pin_obj_t pin_GPIO0;
+extern const mcu_pin_obj_t pin_GPIO1;
+extern const mcu_pin_obj_t pin_GPIO2;
+extern const mcu_pin_obj_t pin_GPIO3;
+extern const mcu_pin_obj_t pin_GPIO4;
+extern const mcu_pin_obj_t pin_GPIO5;
+extern const mcu_pin_obj_t pin_GPIO6;
+extern const mcu_pin_obj_t pin_GPIO7;
+extern const mcu_pin_obj_t pin_GPIO8;
+extern const mcu_pin_obj_t pin_GPIO9;
+
+extern const mcu_pin_obj_t pin_GPIO10;
+extern const mcu_pin_obj_t pin_GPIO11;
+extern const mcu_pin_obj_t pin_GPIO12;
+extern const mcu_pin_obj_t pin_GPIO13;
+extern const mcu_pin_obj_t pin_GPIO14;
+extern const mcu_pin_obj_t pin_GPIO15;
+extern const mcu_pin_obj_t pin_GPIO16;
+extern const mcu_pin_obj_t pin_GPIO17;
+extern const mcu_pin_obj_t pin_GPIO18;
+extern const mcu_pin_obj_t pin_GPIO19;
+
+extern const mcu_pin_obj_t pin_GPIO20;
+extern const mcu_pin_obj_t pin_GPIO21;
+extern const mcu_pin_obj_t pin_GPIO22;
+extern const mcu_pin_obj_t pin_GPIO23;
+extern const mcu_pin_obj_t pin_GPIO24;
+extern const mcu_pin_obj_t pin_GPIO25;
+extern const mcu_pin_obj_t pin_GPIO26;
+extern const mcu_pin_obj_t pin_GPIO27;
+extern const mcu_pin_obj_t pin_GPIO28;
+extern const mcu_pin_obj_t pin_GPIO29;
+
+extern const mcu_pin_obj_t pin_GPIO30;
+extern const mcu_pin_obj_t pin_GPIO31;
+extern const mcu_pin_obj_t pin_GPIO32;
+extern const mcu_pin_obj_t pin_GPIO33;
+extern const mcu_pin_obj_t pin_GPIO34;
+extern const mcu_pin_obj_t pin_GPIO35;
+extern const mcu_pin_obj_t pin_GPIO36;
+extern const mcu_pin_obj_t pin_GPIO37;
+extern const mcu_pin_obj_t pin_GPIO38;
+extern const mcu_pin_obj_t pin_GPIO39;
+
+extern const mcu_pin_obj_t pin_GPIO40;
+extern const mcu_pin_obj_t pin_GPIO41;
+extern const mcu_pin_obj_t pin_GPIO42;
+extern const mcu_pin_obj_t pin_GPIO43;
+extern const mcu_pin_obj_t pin_GPIO44;
+extern const mcu_pin_obj_t pin_GPIO45;
+extern const mcu_pin_obj_t pin_GPIO46;
+extern const mcu_pin_obj_t pin_GPIO47;
+extern const mcu_pin_obj_t pin_GPIO48;
+extern const mcu_pin_obj_t pin_GPIO49;
+
+extern const mcu_pin_obj_t pin_GPIO50;
+extern const mcu_pin_obj_t pin_GPIO51;
+extern const mcu_pin_obj_t pin_GPIO52;
+extern const mcu_pin_obj_t pin_GPIO53;
+#if BCM_VERSION == 2711
+extern const mcu_pin_obj_t pin_GPIO54;
+extern const mcu_pin_obj_t pin_GPIO55;
+extern const mcu_pin_obj_t pin_GPIO56;
+extern const mcu_pin_obj_t pin_GPIO57;
+#endif
